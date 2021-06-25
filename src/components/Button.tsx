a5d563c9635d4678c01ebff76fea4b63cf5d4736
marcosfabio <62723316+marcosfabio@users.noreply.github.com>
2021-06-25 00:07:18 -0300

    criar e Entrar Salas
    Criação e acesso a salas funcionando

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,5 +7,3 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 export function Button(props: ButtonProps) {
   return <button className="button" {...props} />;
 }
-
-export default Button;
